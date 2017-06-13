1d3f7d9562f7bd893bfc6c171f4e4ce2b82f2f31
ocefpaf <ocefpaf@gmail.com>
2017-06-13 15:14:10 -0300

    make soft the default

--- wof/wofpy_config.py
+++ wof/wofpy_config.py
@@ -16,7 +16,7 @@ Generate configuration directory structure for running WOFpy.
 
 Usage:
     wofpy_config INDIR
-    wofpy_config INDIR [--mode=<development,production>] [--overwrite=<soft,hard,none>]
+    wofpy_config INDIR [--mode=<development,production>] [--overwrite=<soft,hard>]
 
     wofpy_config (-h | --help | -v | --version | -m | --mode | -o | --overwrite)
 
@@ -31,9 +31,8 @@ Options:
   -h --help               Show this screen.
   -v --version            Show version.
   -m --mode=development   Deploympent mode [default: development].
-  -o --overwrite=none     Overwrite everything *hard*,
-                          or only adds extra files to an exsting directory *soft*
-                          [default: none].
+  -o --overwrite=soft     Overwrite everything *hard*,
+                          or adds extra files to an exsting directory *soft* [default: soft].
 """
 
 _ROOT = os.path.abspath(os.path.dirname(wof.__file__))
@@ -41,7 +40,7 @@ _CONFIG = os.path.join(_ROOT, 'examples', 'production_configs')
 _ODM2_TIMESERIES = os.path.join(_ROOT, 'examples', 'flask', 'odm2', 'timeseries')
 
 
-def makedirs(directory, overwrite=None):
+def makedirs(directory, overwrite='soft'):
     if not os.path.exists(directory):
         os.makedirs(directory)
     elif os.path.exists(directory) and overwrite in ['soft', 'hard']:
@@ -53,7 +52,7 @@ def makedirs(directory, overwrite=None):
         raise ValueError(msg(directory))
 
 
-def copytree(src, dst, symlinks=False, ignore=None, overwrite='none'):
+def copytree(src, dst, symlinks=False, ignore=None, overwrite='soft'):
     for item in os.listdir(src):
         if '.pyc' in item:
             continue
@@ -70,15 +69,16 @@ def copytree(src, dst, symlinks=False, ignore=None, overwrite='none'):
 def main():
     args = docopt(__doc__, version='1.0.0')
     directory = args.get('INDIR')
-
     mode = args.get('--mode').lower()
     if mode not in ['development', 'production']:
-        raise ValueError('Got mode: {!r}, expected development, or production.'.format(mode))
-
+        raise ValueError(
+            'Got mode: {!r}, expected development, or production.'.format(mode)
+            )
     overwrite = args.get('--overwrite').lower()
-    if overwrite not in ['hard', 'soft', 'none']:
-        msg = 'Got overwrite: {!r}, expected none, hard, or soft.'.format
-        raise ValueError(msg(overwrite))
+    if overwrite not in ['hard', 'soft']:
+        raise ValueError(
+            'Got overwrite: {!r}, expected hard or soft.'.format(overwrite)
+            )
     elif overwrite == 'hard':
         print('Overwriting directory {}.'.format(directory))
     elif overwrite == 'soft':
@@ -88,6 +88,7 @@ def main():
     _TEST = os.path.join(directory, 'odm2', 'timeseries')
     makedirs(_TEST, overwrite)
     copytree(_ODM2_TIMESERIES, _TEST, overwrite=overwrite)
+
     if mode == 'production':
         _PRODUCTION = os.path.join(directory, 'production_configs')
         makedirs(_PRODUCTION, overwrite)
