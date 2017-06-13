0c7e5cce75874f440e8373f377ce516f7b341b19
ocefpaf <ocefpaf@gmail.com>
2017-06-13 11:35:48 -0300

    remove odm2api pinned from git

--- setup.py
+++ setup.py
@@ -55,9 +55,6 @@ setup(
         'sqlite': ['sqlalchemy'],
         'server': ['uwsgi'],
     },
-    dependency_links=[
-        'git+https://github.com/ODM2/ODM2PythonAPI@v0.1.0-alpha#egg=odm2api-0.1.0'  # noqa
-    ],
     tests_require=['suds-jurko', 'requests'],
     classifiers=[
         'Development Status :: 3 - Alpha',
